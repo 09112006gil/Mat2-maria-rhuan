ee132e5c0fc5d969ce4b93112a717403a369a0ef
09112006gil <127346923+09112006gil@users.noreply.github.com>
2024-04-05 10:39:03 -0300

    .

--- main.js
+++ main.js
@@ -50,10 +50,10 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-
+if (tempoFinal > 0){
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-
-
+} else {
+    return "Prazo Finalizado";
 }
 
 
