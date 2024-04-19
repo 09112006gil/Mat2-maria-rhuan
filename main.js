c306533e8acd3dedea5a665e26013057c1d480ce
09112006gil <127346923+09112006gil@users.noreply.github.com>
2024-04-19 10:46:00 -0300

    .

--- main.js
+++ main.js
@@ -23,16 +23,6 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
-function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-    for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i]);   
-    }
-}
 
 function atualizaCronometro(){ 
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
@@ -43,7 +33,10 @@ function atualizaCronometro(){
         //contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
-
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
 
 
 comecaCronometro();
